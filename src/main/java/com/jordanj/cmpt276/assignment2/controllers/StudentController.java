ffa308c9b9381b52253b78e4b54fa7d09d2304dd
ThePixelator0 <jordanjohnswork@gmail.com>
2023-06-07 11:42:46 -0700

    setup project

---
+++ src/main/java/com/jordanj/cmpt276/assignment2/controllers/StudentController.java
@@ -0,0 +1,5 @@
+package com.jordanj.cmpt276.assignment2.controllers;
+
+public class StudentController {
+    
+}
---
+++ src/main/java/com/jordanj/cmpt276/assignment2/models/StudentModel.java
@@ -0,0 +1,5 @@
+package com.jordanj.cmpt276.assignment2.models;
+
+public class StudentModel {
+    
+}
