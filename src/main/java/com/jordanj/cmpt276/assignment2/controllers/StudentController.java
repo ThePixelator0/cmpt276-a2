59bbde49e5e0c40882b4b96a65ab9bca5ca76239
ThePixelator0 <jordanjohnswork@gmail.com>
2023-06-07 14:18:20 -0700

    added student addition form to index.html

--- src/main/java/com/jordanj/cmpt276/assignment2/controllers/StudentController.java
+++ src/main/java/com/jordanj/cmpt276/assignment2/controllers/StudentController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.jordanj.cmpt276.assignment2.models.Student;
 import com.jordanj.cmpt276.assignment2.models.StudentRepository;
@@ -22,9 +24,9 @@ public class StudentController {
         return "students/showAll";
     }
 
-    @GetMapping(value="/students/addStudent")
-    public String addStudent() {
-        Student student = new Student();
+    @PostMapping(value="/students/addStudent")
+    public String addStudent(@RequestParam String name, @RequestParam int weight, @RequestParam int height, @RequestParam String hairColor, @RequestParam double gpa) {
+        Student student = new Student(name, weight, height, hairColor, gpa);
         studentRepo.save(student);
         return "students/added";
     }
